dc4911bc228177a3ac9a020434a724f9f66bfea2
er-Sax <83650953+er-Sax@users.noreply.github.com>
2024-02-05 21:52:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ app.use('/api/user/', userRoutes)
 // Use PORT provided in environment or default to 3000
 const port = process.env.PORT || 3000;
 
-// Listen on `port` and 0.0.0.0
-app.listen(port, "0.0.0.0", function () {
+app.listen(8800, () => {
   console.log('listening')
-});
+})
