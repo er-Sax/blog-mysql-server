70e1a524414f30fb1313a560f25ef8123701ad6f
er-Sax <83650953+er-Sax@users.noreply.github.com>
2024-02-06 23:22:35 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-import mysql from 'mysql2'
+import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-  host: "monorail.proxy.rlwy.net",
-  user: 'root',
-  password: '***REMOVED***',
-  database: 'railway',
-  port: '25634'
-})
+   host: 'monorail.proxy.rlwy.net',
+   user: 'root',
+   password: '***REMOVED***',
+   database: 'railway',
+});
